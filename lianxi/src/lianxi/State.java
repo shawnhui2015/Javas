e4803cb0b3a30a42343ade1c171c8e3760369ec2
shawnhui2015 <momof@58.154.209.181>
2016-04-12 21:58:20 +0800

    w

---
+++ lianxi/src/lianxi/State.java
@@ -0,0 +1,48 @@
+package lianxi;
+
+/**
+ * 进程。
+ * @author 刘鑫伟
+ *
+ */
+public class State {
+	/** 标识 */
+	private String name;
+
+	/*
+	 * 默认构造函数。
+	 */
+	public State() {}
+	
+	/**
+	 * 带参构造函数。
+	 * @param name
+	 */
+	public State(String name) {
+		super();
+		this.name = name;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+}
